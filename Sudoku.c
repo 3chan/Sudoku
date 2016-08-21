fd751ba082d9aa9978cbc947ef133307b794817a
3chan <yusui7500@gmail.com>
2016-08-22 02:06:10 +0900

    ファイル読み込み仮実装、.gitignore追加

--- Sudoku.c
+++ Sudoku.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
 #include "Sudoku.h"
 
+double problem[9][9];  // 問題を保存する配列。0: 空欄。1～9: 問題であらかじめ決められた値。
+double linePattern[9][9 ^ 9];  // 各行における1～9の並べ方を保存する配列。一行の最大パターン数が9^9通りあるためint型の配列では足りない。9 ^ 9 = 387420489。
+
+
+void init() {
+  int i, j;
+  
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) problem[i][j] = 0;
+  }
+
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < (9 * 9); j++) linePattern[i][j] = 0;
+  }
+}
+
+
 int main() {
+  FILE *fp;
+  char *fname = "test.txt";
+  double temp1[9 * 9], temp2;
+  int i, j;
+
+  /* 配列の初期化 */
+  init();
+  
+  /* ファイルオープン */
+  if ((fp = fopen(fname, "r")) == NULL) {
+    printf("Failed to open file \"%s\"\n", fname);
+    return -1;
+  }
+
+  /* ファイル読み込み */
+  j = 0;
+  for (i = 0; i < (10 * 9); i++) {  // (一行9マス + 改行) * 9行
+    temp2 = getc(fp);
+    if (i % 10 != 9) {
+      temp1[j] = temp2;
+      j++;
+    }
+  }
+
+  for (i = 0; i < (9 * 9); i++) {
+    printf("%.0f  ", temp1[i]);
+    if (i % 9 == 8) printf("\n");
+  }
+
+  /* ファイルクローズ */
+  fclose(fp);
+   
   return 0;
 }

83014187110fb4ff5143369673e6a15fb8131c7d
3chan <yusui7500@gmail.com>
2016-08-22 15:07:53 +0900

    1～9の組み合わせを調べる関数を実装中 (1行目のみ)

--- Sudoku.c
+++ Sudoku.c
@@ -3,14 +3,20 @@
 #include "Sudoku.h"
 
 double problem[9][9];  // 問題を保存する配列。0: 空欄。1～9: 問題であらかじめ決められた値。
+double defaultNums[9][9];  // 各マスに問題であらかじめ決められた値が入っているかを登録しておく配列。0: 空欄、1: 問題であらかじめ値が決められているマス。
 double linePattern[9][9 ^ 9];  // 各行における1～9の並べ方を保存する配列。一行の最大パターン数が9^9通りあるためint型の配列では足りない。9 ^ 9 = 387420489。
+
+int pcnt;  // 配列linePattern内のどこまでパターンが保存されているかを指すポインタ
 
 
 void init() {
   int i, j;
   
   for (i = 0; i < 9; i++) {
-    for (j = 0; j < 9; j++) problem[i][j] = 0;
+    for (j = 0; j < 9; j++) {
+      problem[i][j] = 0;
+      defaultNums[i][j] = 0;
+    }
   }
 
   for (i = 0; i < 9; i++) {
@@ -66,15 +72,15 @@ void ReadProblem() {
   for (i = 0; i < 9; i++) {
     for (j = 0; j < 9; j++) {
       problem[i][j] = temp1[k] - 48;  // 16進数から10進数に戻す処理
+      if (problem[i][j] != 0) defaultNums[i][j] = 1;  // 空欄でないマスを登録
       k++;
     }
   }
 
-  /* 問題を出力 */
-  for (i = 0; i < 9; i++) {
-    for (j = 0; j < 9; j++) printf("%.0f  ", problem[i][j]);
-    printf("\n");
-  }
+  /* デバッグ */
+  PrintProblem(problem);
+  printf("defaultNums: \n");
+  PrintProblem(defaultNums);
 
   /* ファイルクローズ */
   fclose(fp);
@@ -83,16 +89,76 @@ void ReadProblem() {
 }
 
 
+void PrintProblem(double prob[][9]) {
+  int i, j;
+  
+  /* 問題を出力 */
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) printf("%.0f  ", prob[i][j]);
+    printf("\n");
+ } 
+}
+
+ 
 void SaveLinePattern() {
   double prob[9][9];
   int i, j,  k;
 
-  for (i = 0; i < 9; i++) {
-    for (j = 0; j < 9; j++) {
-    }
-    CopyProblem(prob);
-  }
+  CopyProblem(prob);  // 配列problemの値をローカルの配列probにコピー  <= 必要か??
 
+  /* デバッグ */
+  printf("CopiedProblem: \n");
+  PrintProblem(prob);
+  printf("\n");
+  
+  Combine1to9(prob[1], 1, 0);  // ひとまず1行目のみ考える
+
+  /* 2行目以降はのちに実装
+  for (i = 0; i < 9; i++) {
+    pcnt = 0;  // 行毎にポインタpcntを初期化
+    Combine1to9(prob[i], i, 0);
+  }
+  */
+  
+  return;
+}
+ 
+ 
+void Combine1to9(double prob[], int line, int index) {
+  int i, j, k, l;
+
+  if (defaultNums[line][index] == 1) {  // あらかじめ空欄でなければ飛ばす
+    index++;
+    Combine1to9(prob, line, index);
+  }
+  
+  for (i = 0; i < 9; i++) {
+    
+    for (j = 0; j < 9; j++) {  // 行内に既にその数字が無いか調べる
+      if (index != j && prob[j] == i) {
+	i++;
+	break;
+      }
+    }
+    
+    if (8 < i) return;
+    else if (index == 8) {  // パターンを保存
+      for (k = 0; k < 9; k++) {
+	linePattern[line][pcnt] = prob[k];
+	pcnt++;
+      }
+      /* デバッグ */
+      printf("prob = {");
+      for (l = 0; l < 9; l++) printf("%.0f, ", prob[l]);
+      printf("}  line = %d, index = %d, i = %d, pcnt = %d\n", line, index, i, pcnt);  // デバッグ
+    }
+    else if (index < 8) {
+      prob[index] = i;
+      index++;
+      Combine1to9(prob, line, index);
+    }
+  }
+  
   return;
 }
 
@@ -105,5 +171,6 @@ void CopyProblem(double prob[][9]) {
       prob[i][j] = problem[i][j];
     }
   }
+
   return;
 }
--- Sudoku.h
+++ Sudoku.h
@@ -1,10 +1,12 @@
 #ifndef __SUDOKU_H__
 #define __SUDOKU_H__
 
-void init();
+void init();  // 配列の初期化を行う
 
-void ReadProblem();
-void SaveLinePattern();
-void CopyProblem(double prob[][9]);
+void ReadProblem();  // ファイルから問題を読み込む
+void PrintProblem(double prob[][9]);  // 数独の配列を標準出力
+void SaveLinePattern();  // 行ごとに取り得る1～9の並びの組み合わせを調べる
+void Combine1to9(double prob[], int line, int index);  // 1～9の並びの組み合わせを調べる
+void CopyProblem(double prob[][9]);  // 問題の配列を他の配列にコピーする
 
 #endif
